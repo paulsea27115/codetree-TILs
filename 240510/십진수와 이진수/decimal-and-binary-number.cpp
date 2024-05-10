3655a44fb652d021ef826adb283412ecd1ad3264
Cho seongbin <92620088+paulsea27115@users.noreply.github.com>
2024-05-10 09:14:50 +0900

    Title: 십진수와 이진수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240510/십진수와 이진수/decimal-and-binary-number.cpp
+++ 240510/십진수와 이진수/decimal-and-binary-number.cpp
@@ -2,17 +2,20 @@
 #include <vector>
 #include <math.h>
 
+#define ll long long int
+
 using namespace std;
+
 
 int main() {
     string bin;
     cin >> bin;
 
-    int gisu = 0;
+    ll gisu = 0;
 
-    int res = 0;
+    ll res = 0;
 
-    for(int i = bin.length()-1; i >= 0; i--){
+    for(ll i = bin.length()-1; i >= 0; i--){
         res += bin[i] == '1' ? pow(2, gisu) : 0;
         gisu += 1;
     }
@@ -21,14 +24,14 @@ int main() {
 
     // cout << res;
 
-    vector<int> bin2;
+    vector<ll> bin2;
 
     while(res > 0) {
         bin2.emplace_back(res % 2);
         res /= 2;
     }
 
-    for(int i = bin2.size()-1; i >= 0; i--){
+    for(ll i = bin2.size()-1; i >= 0; i--){
         cout << bin2[i];
     }
 
