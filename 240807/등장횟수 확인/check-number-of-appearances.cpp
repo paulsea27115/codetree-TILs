8b1ec817999a9f03b1422603209e014c08fdca0f
Cho seongbin <csb070115@gmail.com>
2024-08-07 08:55:44 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240807/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cnt = 0;
+    
+    for(int i = 0; i < 5; i++) {
+        int num;
+        cin >> num;
+
+        if(num % 2 == 0) {
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
